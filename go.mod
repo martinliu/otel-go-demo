3395d2c2e8f98bbbbf8972a661e4806945846782
martinliu <liuzh66@gmail.com>
2021-08-29 20:38:03 +0800

    trace app as whole

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module otel-go-demo
 go 1.16
 
 require (
+	github.com/cucumber/godog v0.8.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/mux v1.8.0
+	github.com/prometheus/procfs v0.0.3 // indirect
 	github.com/rakyll/hey v0.1.4 // indirect
+	go.elastic.co/apm/module/apmgorilla v1.13.1 // indirect
 	golang.org/x/net v0.0.0-20210825183410-e898025ed96a // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- server.go
+++ server.go
@@ -2,6 +2,12 @@
 //流量测试命令如下：
 // hey -z 10m -q 10 -cpus 1 http://localhost:8000/hello/world
 
+//第一步：使用 Elastic APM 的 Go 语言 Agent 埋点
+// git checkout step1
+// hey -z 10m -q 10 -cpus 1 http://localhost:8000/hello/world
+// 查看 APM 的监控界面
+//文档： https://pkg.go.dev/go.elastic.co/apm/module/apmgorilla
+
 package main
 
 import (
@@ -9,6 +15,9 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+
+	//第一步
+	"go.elastic.co/apm/module/apmgorilla"
 )
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
@@ -17,6 +26,7 @@ func helloHandler(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	r := mux.NewRouter()
+	r.Use(apmgorilla.Middleware())
 	r.HandleFunc("/hello/{name}", helloHandler)
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
